303b10d919f0c1c27a1a293f2f08288b9d4a2c23
Shital Chandore <Shital.Chandore@ust.com>
2022-10-02 17:16:19 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { hot } from 'react-hot-loader';
+
+const App = () => (
+	<div className="App">
+		<h1>Hello,World</h1>
+		<h1>Hello,World</h1>
+		<h1>Hello,World</h1>
+		<h1>Hello,World ok shital its done</h1>
+		<h5>hellow from mars!!!</h5>
+	</div>
+);
+export default hot(module)(App);
